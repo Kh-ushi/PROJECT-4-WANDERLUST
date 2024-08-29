a27549c7ae9c1b5c87ca8e93572a60cee06159e4
Kh-ushi <115355440+Kh-ushi@users.noreply.github.com>
2024-08-29 15:30:31 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -1,62 +1,119 @@
-const express=require("express");
-const router=express.Router();
-const wrapAsync=require("../utils/wrapAsync");
-const {listingSchema}=require("../schema");
-const ExpressError=require("../utils/ExpressError");
-const Listing=require("../models/listing");
-const{isLoggedIn,isOwner}=require("../middleware");
-const multer  = require('multer');
-const {storage}=require("../cloudConfig");
-const upload = multer({ storage});
+// const express=require("express");
+// const router=express.Router();
+// const wrapAsync=require("../utils/wrapAsync");
+// const {listingSchema}=require("../schema");
+// const ExpressError=require("../utils/ExpressError");
+// const Listing=require("../models/listing");
+// const{isLoggedIn,isOwner}=require("../middleware");
+// const multer  = require('multer');
+// const {storage}=require("../cloudConfig");
+// const upload = multer({ storage});
 
 
-//MVC
-const{indexRoute,newRoute,showRoute,createRoute,editRoute,updateRoute,deleteRoute}=require("../controller/listings");
+// //MVC
+// const{indexRoute,newRoute,showRoute,createRoute,editRoute,updateRoute,deleteRoute}=require("../controller/listings");
 
 
 
-//SERVER SIDE VALIDATION
-const validateListing=(res,req,next)=>{
+// //SERVER SIDE VALIDATION
+// const validateListing=(res,req,next)=>{
+//     let result = listingSchema.validate(req.body);
+//     if(result.error){
+//       throw new ExpressError(400,result.error);
+//     }
+//      else{
+//         next();
+//      }
+// }
+
+// //INDEX AND CREATE ROUTE
+// router.route("/")
+// .get(wrapAsync(indexRoute))
+// .post(isLoggedIn,upload.single('listing[image][url]'),validateListing,wrapAsync(createRoute));
+
+// //SEARCH BAR
+// router.get("/",(req,res)=>{
+//      res.redirect("/listings");
+// });
+
+// router.post("/searchDest",wrapAsync(async (req,res)=>{
+   
+//   let searchData=req.body.listing.country;
+  
+//   let dataSearch=await Listing.find({country:searchData});
+  
+//   res.render("listings/index.ejs",{dataSearch});
+  
+//   }));
+
+// //NEW ROUTE
+// router.get("/new",isLoggedIn,newRoute);
+
+// //SHOW UPDATE DELETE
+// router.route("/:id")
+// .get(wrapAsync(showRoute))
+// .put(isLoggedIn,isOwner,upload.single('listing[image][url]'),validateListing, wrapAsync(updateRoute))
+// .delete(isLoggedIn,isOwner,wrapAsync(deleteRoute));
+
+
+//  //EDIT ROUTE
+//  router.get("/:id/edit", isLoggedIn,isOwner,wrapAsync(editRoute));
+
+//  module.exports=router;
+
+
+
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync");
+const { listingSchema } = require("../schema");
+const ExpressError = require("../utils/ExpressError");
+const Listing = require("../models/listing");
+const { isLoggedIn, isOwner } = require("../middleware");
+const multer = require('multer');
+const { storage } = require("../cloudConfig");
+const upload = multer({ storage });
+
+// MVC
+const { indexRoute, newRoute, showRoute, createRoute, editRoute, updateRoute, deleteRoute } = require("../controller/listings");
+
+// SERVER SIDE VALIDATION
+const validateListing = (req, res, next) => {
     let result = listingSchema.validate(req.body);
-    if(result.error){
-      throw new ExpressError(400,result.error);
-    }
-     else{
+    if (result.error) {
+        throw new ExpressError(400, result.error);
+    } else {
         next();
-     }
-}
+    }
+};
 
-//INDEX AND CREATE ROUTE
+// INDEX AND CREATE ROUTE
 router.route("/")
-.get(wrapAsync(indexRoute))
-.post(isLoggedIn,upload.single('listing[image][url]'),validateListing,wrapAsync(createRoute));
+    .get(wrapAsync(indexRoute))
+    .post(isLoggedIn, upload.single('listing[image][url]'), validateListing, wrapAsync(createRoute));
 
-//SEARCH BAR
-router.get("/",(req,res)=>{
-     res.redirect("/listings");
+// SEARCH BAR
+router.get("/", (req, res) => {
+    res.redirect("/listings");
 });
 
-router.post("/searchDest",wrapAsync(async (req,res)=>{
-   
-  let searchData=req.body.listing.country;
-  
-  let dataSearch=await Listing.find({country:searchData});
-  
-  res.render("listings/index.ejs",{dataSearch});
-  
-  }));
+router.post("/searchDest", wrapAsync(async (req, res) => {
+    let searchData = req.body.listing.country;
+    let dataSearch = await Listing.find({ country: searchData });
+    res.render("listings/index.ejs", { dataSearch });
+}));
 
-//NEW ROUTE
-router.get("/new",isLoggedIn,newRoute);
+// NEW ROUTE
+router.get("/new", isLoggedIn, newRoute);
 
-//SHOW UPDATE DELETE
+// SHOW UPDATE DELETE
 router.route("/:id")
-.get(wrapAsync(showRoute))
-.put(isLoggedIn,isOwner,upload.single('listing[image][url]'),validateListing, wrapAsync(updateRoute))
-.delete(isLoggedIn,isOwner,wrapAsync(deleteRoute));
+    .get(wrapAsync(showRoute))
+    .put(isLoggedIn, isOwner, upload.single('listing[image][url]'), validateListing, wrapAsync(updateRoute))
+    .delete(isLoggedIn, isOwner, wrapAsync(deleteRoute));
 
+// EDIT ROUTE
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(editRoute));
 
- //EDIT ROUTE
- router.get("/:id/edit", isLoggedIn,isOwner,wrapAsync(editRoute));
+module.exports = router;
 
- module.exports=router;
